e01fc0c08157e00770db7efa95f9b8a78add6168
rexsimiloluwah <adetoyosi2001@gmail.com>
2021-10-10 18:24:34 +0100

    Removed dist folder

--- nodejs/dist/db/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _pgClient = _interopRequireDefault(require("./pgClient"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-_pgClient["default"].query("SELECT * FROM users").then(function (res) {
-  console.log(res);
-});
